7acfc8e6bd7388f27a438058418cd6ecc6aa607a
zhanghao1992 <386708307@qq.com>
2017-12-24 16:56:09 +0800

    amin

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
     <el-container>
-      <el-header>中棋监控平台</el-header>
+      <el-header>
+        中棋监控平台
+        <div class="loginOut" v-if="user">
+          <span>管理员：{{user.username}}</span>
+          <a href="javascript:void(0)">退出</a>
+        </div>
+      </el-header>
       <router-view/>
     </el-container>
   </div>
 </template>
 
 <script>
+  import {
+    mapGetters
+  } from 'vuex'
+
   export default {
-    name: 'app'
+    name: 'app',
+    computed: {
+      ...mapGetters(['user'])
+    }
   }
 </script>
 
-<style>
+<style lang="scss">
   html {
     min-height: 100%;
   }
@@ -29,6 +42,8 @@
     font-weight: 700;
     color: #fff;
     line-height: 60px;
+    padding-left: 80px;
+    position: relative;
   }
 
   .el-table .cell, .el-table th div, .el-table--border td:first-child .cell, .el-table--border th:first-child .cell {
@@ -37,5 +52,23 @@
 
   .el-table .cell {
     line-height: 24px;
+  }
+
+  .loginOut {
+    position: absolute;
+    right: 100px;
+    top: 0;
+    bottom: 0;
+    line-height: 60px;
+    font-size: 20px;
+    font-weight: 400;
+    a {
+      margin-left: 30px;
+      color: #fff;
+      font-size: 16px;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+// import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+import './assets/css/reset.css'
 import App from './App'
 import router from './router'
 import Element from 'element-ui'
